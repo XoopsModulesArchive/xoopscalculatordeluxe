763e4053136174c115d132bbda5b183d89a0d1a0
mambax7 <mambax7@gmail.com>
2020-07-11 22:49:56 -0400

    initial
    Signed-off-by: mambax7 <mambax7@gmail.com>

---
+++ index.php
@@ -0,0 +1,75 @@
+<?php
+include("../../mainfile.php");
+include(XOOPS_ROOT_PATH."/header.php");
+include("header.php");
+
+$xoopsOption['show_rblock'] = 1;
+?>
+
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<title>xoopscalculatordeluxe</title>
+<script language="javascript">AC_FL_RunContent = 0;</script>
+<script src="AC_RunActiveContent.js" language="javascript"></script>
+</head>
+<body bgcolor="#ffffff"><div align="center">
+<!--url's used in the movie-->
+<a href="http://xoops.org/"></a>
+<!--text used in the movie-->
+<!--
+<p align="left"><font face="Trebuchet MS" size="10" color="#999999" letterSpacing="0.000000" kerning="0">Ported to <a href="http://www.xoops.org/" target = "_blank">Xoops</a> by &nbsp;<a href="http://www.spacedesign.nu" target = "_blank">Spacedesign</a> </font></p>
+deg 
+dec 
+off
+-->
+<!-- saved from url=(0013)about:internet -->
+<script language="javascript">
+	if (AC_FL_RunContent == 0) {
+		alert("This page requires AC_RunActiveContent.js.");
+	} else {
+		AC_FL_RunContent(
+			'codebase', 'http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=6,0,0,0',
+			'width', '400',
+			'height', '600',
+			'src', 'xoopscalculatordeluxe',
+			'quality', 'high',
+			'pluginspage', 'http://www.macromedia.com/go/getflashplayer',
+			'align', 'middle',
+			'play', 'true',
+			'loop', 'true',
+			'scale', 'showall',
+			'wmode', 'window',
+			'devicefont', 'false',
+			'id', 'xoopscalculatordeluxe',
+			'bgcolor', '#ffffff',
+			'name', 'xoopscalculatordeluxe',
+			'menu', 'true',
+			'allowFullScreen', 'false',
+			'allowScriptAccess','sameDomain',
+			'movie', 'xoopscalculatordeluxe',
+			'salign', ''
+			); //end AC code
+	}
+</script>
+<div align="center"><noscript>
+	<object classid="clsid:d27cdb6e-ae6d-11cf-96b8-444553540000" codebase="http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=6,0,0,0" width="400" height="600" id="xoopscalculatordeluxe" align="middle">
+	<param name="allowScriptAccess" value="sameDomain" />
+	<param name="allowFullScreen" value="false" />
+	<param name="movie" value="xoopscalculatordeluxe.swf" /><param name="quality" value="high" /><param name="bgcolor" value="#ffffff" />	<embed src="xoopscalculatordeluxe.swf" quality="high" bgcolor="#ffffff" width="400" height="600" name="xoopscalculatordeluxe" align="middle" allowScriptAccess="sameDomain" allowFullScreen="false" type="application/x-shockwave-flash" pluginspage="http://www.macromedia.com/go/getflashplayer" />
+	</object>
+</noscript></div>
+</body>
+</html>
+
+
+
+
+
+
+
+
+<?php
+include(XOOPS_ROOT_PATH."/footer.php");
+
+?>
---
+++ xoops_version.php
@@ -0,0 +1,16 @@
+<?php
+$modversion['name'] = "Calculator Deluxe";
+$modversion['version'] = 1.01;
+$modversion['description'] = "Advanced flash calculator with convertion tools and changable skins.";
+$modversion['author'] = "Ported To Xoops By Spacedesign (<a href='http://spacedesign.nu/'>Spacedesign</a>)";
+$modversion['credits'] = "";
+$modversion['help'] = "";
+$modversion['license'] = "GPL see LICENSE";
+$modversion['official'] = 0;
+$modversion['image'] = "images/xoopscalculatordeluxe.gif";
+$modversion['dirname'] = "xoopscalculatordeluxe";
+$modversion['hasAdmin'] = 0;
+$modversion['adminpath'] = "";
+$modversion['hasMain'] = 1;
+
+?>
